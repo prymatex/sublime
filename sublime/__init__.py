60f0b6d5946f0244c4bbb59956731504e47b05a5
Diego van Haaster <diegomvh@gmail.com>
2015-03-13 19:07:26 -0300

    Settings

--- sublime/__init__.py
+++ sublime/__init__.py
@@ -18,33 +18,23 @@ from .window import Window
 
 INHIBIT_WORD_COMPLETIONS = False
 INHIBIT_EXPLICIT_COMPLETIONS = False
-timer_thread_name = "Sublime Timer"
 
 pmx = qapplication()
-DESCRIPTOR = None
 SETTINGS = {}
-
-def setup_sublime_adapter(manager, descriptor):
-    global DESCRIPTOR, SETTINGS
-    
-    # Descriptor
-    DESCRIPTOR = descriptor
-    
-    # Settings
-    SETTINGS["Preferences.sublime-settings"] = Settings(pmx.profile().settings)
 
 def load_settings(base_name):
     if base_name not in SETTINGS:
-        path = os.path.join(DESCRIPTOR.path, base_name)
-        settings = json.read_file(path)
-        if settings:
-            SETTINGS[base_name] = Settings(PrymatexSettings(base_name, settings))
+        if base_name == "Preferences.sublime-settings":
+            SETTINGS[base_name] = Settings(pmx.profile().settings)
+        else:
+            path = os.path.join(DESCRIPTOR.path, base_name)
+            settings = json.read_file(path)
+            if settings:
+                SETTINGS[base_name] = Settings(PrymatexSettings(base_name, settings))
     return SETTINGS[base_name]
 
 def active_window():
     return Window(pmx.currentWindow())
 
-def set_timeout(callback, delay, *args, **kwargs):
-    timer = threading.Timer(delay / 1000, callback, args, kwargs)
-    timer.name = timer_thread_name
-    timer.start()
+def set_timeout(callback, delay):
+    pmx.setTimeout(delay / 1000, callback)
